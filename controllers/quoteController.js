1d20c84e8e83109caa4ea07fbed42b53083e3dd4
Ashar Ali <ashar.alia1999@gmail.com>
2021-05-02 23:38:57 +0500

    unique id and few code refactoring

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
-const { readFile } = require("../utility/common");
+const {v4:uuid} = require('uuid')
+const { readFile,writeFile } = require("../utility/common");
 exports.fetchQuotes = async(req, res) =>{
     try {
         var quotes = await readFile(`${__dirname}/../data/quotes.json`,'utf-8')
-        console.log(quotes)
         res.status(200).json({
             status:"success",
             data:{
@@ -17,21 +16,21 @@ exports.fetchQuotes = async(req, res) =>{
 exports.addQuotes = async(req, res) =>{
     try {
         //take out data from body req
-        var{title, quote, author} = req.body;
+        var{id, title, quote, author} = req.body;
 
         //fetch file
         var quotes = await readFile(`${__dirname}/../data/quotes.json`,'utf-8')
         //push new quote
-        quotes.push({
+        var newQuotes = {
+            id: uuid(), 
             title,
             quote,
             author
-        })
+        }
+        console.log(newQuotes)
+        quotes.push(newQuotes)
         // write file with updated data
-        fs.writeFile(`${__dirname}/../data/quotes.json`,JSON.stringify(quotes),(err) =>{
-            if (err) console.log(err)
-            console.log("file written")
-        })
+        await writeFile(`${__dirname}/../data/quotes.json`,quotes)
         //send back response
         res.status(200).json({
             msg:"add quote"
--- utility/common.js
+++ utility/common.js
@@ -8,3 +8,11 @@ exports.readFile = (path, format) =>{
         })
     })
 }
+exports.writeFile = (path, data) =>{
+    return new Promise((res,rej) =>{
+        fs.writeFile(path,JSON.stringify(data),(err) =>{
+            if (err) rej(err)
+            res("done")
+        })
+    })
+}
